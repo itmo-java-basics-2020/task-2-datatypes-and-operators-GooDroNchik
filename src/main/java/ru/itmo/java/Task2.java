f00572f137af35c582788c4b86ffac5c19668b22
GooDroN <lord_1998@mail.ru>
2020-02-22 09:32:21 +0300

    update

--- src/main/java/ru/itmo/java/Task2.java
+++ src/main/java/ru/itmo/java/Task2.java
@@ -11,21 +11,23 @@ public class Task2 {
      * - вывод 0.9
      */
     double getFractionalPart(double realNumber) {
-        return 0.0; //your code here
+        double result = realNumber - (int)realNumber;
+        return result;
     }
 
     /**
      * Дан символ, верните его код. Не используйте явное приведение типов и любые библиотечные функции
      */
     int charCode(char c) {
-        return 0; //your code here
+        return c; //your code here
     }
 
     /**
      * Даны 2 целых числа. Проверьте равны ли они
      */
     boolean testIfIntsEqual(Integer a, Integer b) {
-        return false; //your code here
+        return a.equals(b);
+        //your code here
     }
 
     /**
@@ -45,28 +47,43 @@ public class Task2 {
      * Даны 3 символа. Определите является ли хотя бы один из них цифрой 1..9
      */
     boolean atLeastOneIsDigit(char c1, char c2, char c3) {
-        return false; //your code here
+        if (Character.isDigit(c1) == true | Character.isDigit(c2) == true | Character.isDigit(c3) == true){
+            return true;
+        } else{
+            return false;
+        }
+         //your code here
     }
 
     /**
      * Даны два вещественных числа. Определите равны ли они
      */
     boolean areRealNumbersEqual(double a, double b) {
-        return false; //your code here
+        if (Double.compare(a, b) == 0){
+            return true;
+        } else{
+            return false;
+        }
+        //your code here
     }
 
     /**
      * Дано трехзначное число 100 <= N <= 999. Найдите сумму его чисел.
      */
     int sumOfDigits(int n) {
-        return 0; //your code here
+        Integer sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum; //your code here
     }
 
     /**
      * Дано натуральное число N <= 10000. Выведите следующее после N четное число. Например: N = 8, Result = 10
      */
     int nextEvenNumber(int n) {
-        return 0; //your code here
+        return n+2; //your code here
     }
 
     /**
@@ -76,14 +93,26 @@ public class Task2 {
      * своем кабинете
      */
     int schoolDesks(int num1, int num2, int num3) {
-        return 0; //your code here
+        int sum = 0;
+        int[] classes = {num1, num2, num3};
+        for (int num : classes){
+            if ((num % 2) == 0){
+                sum += num / 2;
+            } else{
+                sum += num / 2 + 1;
+            }
+        }
+        return sum; //your code here
     }
 
     /**
      * Дано натуральное число N >= 10. Выведите результат применения XOR к последним 2-м цифрам числа N
      */
     int xorDigits(int N) {
-        return 0; //your code here
+        int first_num = N % 10;
+        N = N / 10;
+        int second_num = N % 10;
+        return first_num ^ second_num; //your code here
     }
 
 }
